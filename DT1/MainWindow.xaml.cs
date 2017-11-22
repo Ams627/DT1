d6d30dcebe07f3a5cebd5230ee62a722399de508
Ams <adriansims62781@gmail.com>
2017-11-22 22:35:17 +0000

    Add project files.

---
+++ DT1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace DT1
+{
+    public class X
+    {
+    }
+
+    public partial class MainWindow : Window
+    {
+        public List<X> List { get; set; } = new List<X>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            List.Add(new X());
+            List.Add(new X());
+            this.DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var items = icontrol1.Items;
+            for (var i = 0; i < items.Count; ++i)
+            {
+                var item = (UIElement)icontrol1.ItemContainerGenerator.ContainerFromIndex(i);
+                var cp2 = VisualTreeHelper.GetChild
+                if (item is ContentPresenter cp)
+                {
+                    cp.ApplyTemplate();
+                    var template = cp.ContentTemplate;
+                    System.Diagnostics.Debug.WriteLine($"template is {template?.ToString() ?? "NULL"}");
+                }
+            }
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,16 @@
+rem echo off
+git init
+git add DT1/App.xaml
+git add DT1/App.xaml.cs
+git add DT1/App.config
+git add DT1/MainWindow.xaml
+git add DT1/MainWindow.xaml.cs
+git add DT1/Properties/AssemblyInfo.cs
+git add DT1/app.config
+git add DT1/Properties/Settings.settings
+git add DT1/Properties/Resources.Designer.cs
+git add DT1/Properties/Resources.resx
+git add DT1/Properties/Settings.Designer.cs
+git add DT1/DT1.csproj
+git add DT1.sln
+git add .gitignore
